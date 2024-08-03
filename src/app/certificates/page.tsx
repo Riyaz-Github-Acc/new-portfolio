2ff5c62eb6bf0462479e8876a4ea280385e7d035
Riyaz <riyazofficeid@gmail.com>
2024-08-03 23:11:31 +0530

    certificates section design completed

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageHeader from '@/components/pageHeader/PageHeader'
+import { CertificatesCard } from '@/components/certificatesCard/certificatesCard'
 
 const Certificates = () => {
   return (
     <section className='page-container'>
       <PageHeader title='Certificates' />
+      <CertificatesCard />
     </section>
   )
 }
